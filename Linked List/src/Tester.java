83ea0aa7d0a4626d7aaed19e9259a0342e7381f2
chorro73 <chorro73@gmail.com>
2016-01-17 16:12:56 -0800

    Implement add functionality to interface
    Found bug: adding to an empty linked list get index out of bounds
    exception.

--- Linked List/src/Tester.java
+++ Linked List/src/Tester.java
@@ -1,22 +1,29 @@
 import java.util.Scanner;
 
 public class Tester {
+	private static Scanner sc = new Scanner(System.in);
+	private static String in = "";
+	private static MyLinkedList list;
+
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
 		System.out.println("Starting List Tester:");
 		System.out.println("Start with a pre-build list? (y/n)");
 		if (sc.nextLine().equalsIgnoreCase("y")) {
-			MyLinkedList list = new MyLinkedList(1);
+			list = new MyLinkedList(1);
 			list.add(3);
 			list.add(100);
 			System.out.println(list);
+		} else {
+			list = new MyLinkedList();
 		}
+
 		boolean done = false;
 		while (!done) {
+			System.out.println("The list is currently: " + list);
 			System.out.println("Select an option [A]dd, [G]et, [D]one");
-			String in = sc.nextLine();
+			in = sc.nextLine();
 			if (in.equalsIgnoreCase("a")) {
-				// add
+				addMenu();
 			} else if (in.equalsIgnoreCase("g")) {
 				// get
 			} else if (in.equalsIgnoreCase("d")) {
@@ -24,5 +31,12 @@ public class Tester {
 			}
 		}
 		sc.close();
+	}
+
+	private static void addMenu() {
+		System.out
+				.println("Enter an integer Value to end to the end of the list:");
+		int in = sc.nextInt();
+		list.add(in);
 	}
 }
