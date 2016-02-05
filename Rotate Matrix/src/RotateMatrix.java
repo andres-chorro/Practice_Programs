ae84e56b2a1c630ddfa265de10c035928b7e36f1
chorro73 <chorro73@gmail.com>
2016-02-04 19:17:04 -0800

    Added generator

--- Rotate Matrix/src/RotateMatrix.java
+++ Rotate Matrix/src/RotateMatrix.java
@@ -2,21 +2,24 @@
 public class RotateMatrix {
 	public static void main(String[] args) {
 		int[][] test = {{3,3},{3,3}};
-		printArray(rotate(test));
+		//printArray(rotate(test));
+		printArray(generateScreen(1));
+		printArray(generateScreen(4));
+		printArray(generateScreen(5));
+		printArray(generateScreen(0));
+
 	}
 	
 	public static int[][] rotate(int[][] matrix) {
 		if (matrix.length != matrix[0].length)
 			return null;
-		int x, y, min, tmp = 0;
+		int x, y, min, tmp;
+		x = y = min = tmp = 0;
 		int max = matrix.length - 1;
 		while (min < max) {
 			for (int i = min; i <= max; i++) {
 				for (int j = 0; j < 4; j++) {
-					tmp = matrix[max-y][x];
-					matrix[max-y][x] = matrix[x][y];
-					x = max - y;
-					y = x;
+					
 				}
 			}
 		}
@@ -34,5 +37,17 @@ public class RotateMatrix {
 			}
 			System.out.println();
 		}
+	}
+	
+	private static int[][] generateScreen(int length) {
+		int counter = 0;
+		int [][] result = new int[length][length];
+		for (int i = 0; i < length; i++) {
+			for (int j = 0; j < length; j++){
+				result[i][j] = counter;
+				counter++;
+			}
+		}
+		return result;
 	}
 }
