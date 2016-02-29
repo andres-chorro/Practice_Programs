bc9877bd7ae891631722100167bb27219c6be500
chorro73 <chorro73@gmail.com>
2016-02-28 20:19:36 -0800

    Started Stack/Queue

---
+++ My Queue/src/MyQueue.java
@@ -0,0 +1,16 @@
+
+public class MyQueue {
+	private int stack[];
+	private int queue[];
+	private int size;
+	
+	public void enqueue(int value){
+		
+	}
+	
+	public int dequeue() {
+		if (size < 1)
+			return -1;
+		
+	}
+}
